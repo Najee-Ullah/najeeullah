1ca9ab41a8f5b0247d93da36ecf16e5e3f5fa947
Koni <najeeullahnoon786@gmail.com>
2025-10-16 09:41:28 +0500

    feat: add contact page

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 async function loadProjects() {
   try {
-    const response = await fetch("projects.json");
-    const projects = await response.json();
+    const response = await fetch("data.json");
+    const data = await response.json();
+
+    const projects = data.projects;
 
     const grid = document.getElementById("projectsGrid");
 
@@ -32,4 +34,24 @@ async function loadProjects() {
   }
 }
 
-loadProjects();
+async function loadSocials() {
+  try {
+    const response = await fetch("data.json");
+    const data = await response.json();
+    const socialLinks = data.socials;
+    const container = document.getElementById("socialGrid");
+
+    socialLinks.forEach(social => {
+      const link = document.createElement("a");
+      link.href = social.link;
+      link.target = "_blank";
+      link.classList.add("social-item");
+      link.innerHTML = `
+        <img src="${social.icon}" alt="${social.name}" class="social-icon">
+      `;
+      container.appendChild(link);
+    });
+  } catch (error) {
+    console.error("Error loading social links:", error);
+  }
+}
